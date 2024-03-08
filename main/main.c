00cc8bc3c842627e586850c6284bd91ca4b032c6
gitSG <47866306+gitSG@users.noreply.github.com>
2024-03-08 20:22:34 +0530

    T100-add function-attempt2

--- main/main.c
+++ main/main.c
@@ -2,10 +2,15 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+int add(int a, int b)
+{
+	return a+b;
+}
 void app_main(void)
 {
     while (true) {
         printf("Hello from app_main!\n");
         sleep(1);
+        int result = add(2, 3);
     }
 }
